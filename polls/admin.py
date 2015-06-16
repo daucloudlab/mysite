a8230b7adc878fe1cb2050ba00377791638c7c78
Daulet Baimbetov <daucloudlab@gmail.com>
2015-06-16 17:55:02 +0600

    (version 01) модел admin бетіне қосылды

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from polls.models import Question
 # Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+
+
