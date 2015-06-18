adcd31d0fe25f330c1606369fba780f32796fd62
Daulet Baimbetov <daucloudlab@gmail.com>
2015-06-18 11:24:57 +0600

    (version 01) http://localhost:8000/polls істеп тұр

---
+++ polls/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from polls.views import index, detail, results, vote
+
+urlpatterns = [
+    url(r"^$", index, name = "index"),
+    url(r'^(?P<question_id>[0-9]+)/$', detail, name = "detail"),
+    url(r'^(?P<question_id>\d[0-9]+)/results/$', results, name = 'results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', vote, name = 'vote'),
+]
